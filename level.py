e59ed318f5f6eda999d05aac0229be89328fb08e
nixpie <adriantrojanowski@protonmail.com>
2023-01-12 19:23:39 +0100

    Particles finish

--- level.py
+++ level.py
@@ -4,7 +4,7 @@ from tile import Tile
 from player import Player
 from debug import debug
 from support import *
-from random import choice
+from random import choice,randint
 from weapon import Weapon
 from ui import UI
 from enemy import Enemy
@@ -85,7 +85,8 @@ class Level:
                                     (x,y),
                                     [self.visible_sprites,self.attackable_sprites],
                                     self.obstacle_sprites,
-                                    self.damage_player)
+                                    self.damage_player,
+                                    self.trigger_death_particles)
 
     def create_attack(self):
         self.current_attack = Weapon(self.player, [self.visible_sprites,self.attack_sprites])
@@ -108,7 +109,9 @@ class Level:
                     for target_sprite in collision_sprites:
                         if target_sprite.sprite_type == 'grass':
                             pos = target_sprite.rect.center
-                            self.animation_player.create_grass_particles(pos,[self.visible_sprites])
+                            offset = pygame.math.Vector2(0,75)
+                            for leaf in range(randint(3,6)):
+                                self.animation_player.create_grass_particles(pos - offset,[self.visible_sprites])
                             target_sprite.kill()
                         else:
                             target_sprite.get_damage(self.player,attack_sprite.sprite_type)
@@ -118,7 +121,11 @@ class Level:
             self.player.health -= amount
             self.player.vulnerable = False
             self.player.hurt_time = pygame.time.get_ticks()
-            # spawn particles
+            self.animation_player.create_particles(attack_type,self.player.rect.center,[self.visible_sprites])
+
+    def trigger_death_particles(self,pos,particle_type):
+
+        self.animation_player.create_particles(particle_type,pos,self.visible_sprites)
 
     def run(self):
         # update and draw the game
--- particles.py
+++ particles.py
@@ -52,6 +52,9 @@ class AnimationPlayer:
         animation_frames = choice(self.frames['leaf'])
         ParticleEffect(pos,animation_frames,groups)
 
+    def create_particles(self,animation_type,pos,groups):
+        animation_frames = self.frames[animation_type]
+        ParticleEffect(pos,animation_frames,groups)
 
 class ParticleEffect(pygame.sprite.Sprite):
     def __init__(self,pos,animation_frames,groups):
